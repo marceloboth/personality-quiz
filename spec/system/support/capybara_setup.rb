39058f865a49805ddd8bfb7ad06b14b1b79ca9e6
Marcelo Both <marcelo.both@gmail.com>
2023-01-25 15:00:40 -0300

    test: small adjust to make capybara able to click on radio button labels.

--- spec/system/support/capybara_setup.rb
+++ spec/system/support/capybara_setup.rb
@@ -13,6 +13,9 @@ Capybara.default_max_wait_time = 2
 # That makes tests less dependent on slightly UI changes.
 Capybara.default_normalize_ws = true
 
+# Usually capybara doesn't click a label for a radio button
+Capybara.automatic_label_click = true
+
 # Where to store system tests artifacts (e.g. screenshots, downloaded files, etc.).
 # It could be useful to be able to configure this path from the outside (e.g., on CI).
 Capybara.save_path = ENV.fetch('CAPYBARA_ARTIFACTS', './tmp/capybara')
